d4f7ad125c848420941daef60f7992751c68d3ac
Natapat Samutpong <natapat.samutpong@gmail.com>
2022-03-15 11:43:23 +0700

    misc

--- src/ui.rs
+++ src/ui.rs
@@ -1,8 +1,1 @@
 pub const BANNER: &str = r"Welcome to Xornet Terminal Client!";
-
-pub const RESET: &str   = "\x1b[0m";
-pub const RED: &str     = "\x1b[31m";
-pub const GREEN: &str   = "\x1b[32m";
-pub const YELLOW: &str  = "\x1b[33m";
-pub const BLUE: &str    = "\x1b[34m";
-pub const MAGENTA: &str = "\x1b[35m";
